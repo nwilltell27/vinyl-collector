fb0aae50d026f51080c8e51e24e55bd17bfef2dd
Nathan Tellers <nathantellers@gmail.com>
2021-07-06 20:52:32 -0700

    wip: adds styling, home page, and index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('view_vinyl/', views.vinyl_index, name='index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+class Record:
+    def __init__(self, artist, album_name, genre, release_year):
+        self.artist = artist
+        self.album_name = album_name
+        self.genre = genre
+        self.release_year = release_year
+
+vinyl = [
+    Record('Lettuce', 'Resonate', 'Funk', 2020),
+    Record('Vulfpeck', 'Live at Madison Square Garden', 'Funk', 2019),
+    Record('Pigeons Playing Ping Pong', 'Pizazz', 'Funk', 2017),
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def vinyl_index(request):
+    return render(request, 'vinyl/index.html', { 'vinyl': vinyl })
